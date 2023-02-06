47526cc1881fbe6f092953e6ad75d2cd0f006d2d
Winter <mywinter.h@gmail.com>
2023-02-06 10:27:23 -0800

    update measurement

--- src/cpu/measure_task.c
+++ src/cpu/measure_task.c
@@ -4,25 +4,33 @@
 #include <unistd.h>
 #include <math.h>
 #include <pthread.h>
-
-#define SIZE_OF_STAT 10
-#define BOUND_OF_LOOP 10
+#include "../utils/constant.h"
+#include "../utils/calculation.h"
 
 void *thread(void *arg) {
     pthread_exit(NULL);
 }
 
 int main() {
+    uint64_t **times;
+    times = malloc((BOUND_OF_LOOP + 1)* sizeof(uint64_t*)); // omit the first batch for warm cache
+    if(!times) {
+        printf("failed to allocate memory to times...\n");
+        return 0;
+    }
+    for(int i = 0; i <= BOUND_OF_LOOP; i++) {
+        times[i] = malloc(SIZE_OF_STAT * sizeof(uint64_t));
+        if(!times[i]) {
+            printf("failed to allocate memory to times[%d]...\n", i);
+            for(int k = 0; k < i; k++) free(times[k]);
+            return 0;
+        }
+    }
     unsigned int cycles_low0, cycles_high0, cycles_low1, cycles_high1;
     uint64_t start, end;
-    double means[BOUND_OF_LOOP];
-    double mean_all = 0;
-    double sd = 0;
-    double sum_temp = 0;
     printf("=========== Measuring Process Creation ===========\n");
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        printf("Iter[%d]\n", i);
-        uint64_t sum = 0;
+    for (int i = 0; i <= BOUND_OF_LOOP; i += 1) {
+        //printf("Iter[%d]\n", i);
         // unrolling required
         for (int j = 0; j < SIZE_OF_STAT; j += 1) {
             __asm__ volatile (
@@ -46,31 +54,34 @@ int main() {
             }
             start = (((uint64_t)cycles_high0 << 32) | cycles_low0);
             end = (((uint64_t)cycles_high1 << 32) | cycles_low1);
-            sum += (end - start);
-            printf("%lu ", end - start);
-            if (j != 0 && j % 20 == 0) {
-                printf("\n");
+            if((end - start) < 0) {
+                printf("wrong timing: start:%lu, end:%lu ...\n", start, end);
+                times[i][j] = 0;
+            } else {
+                times[i][j] = end - start;
             }
         }
-        means[i] = (double)sum / SIZE_OF_STAT;
-        printf("\nIter[%d] mean: %f\n", i, means[i]);
     }
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        mean_all += means[i];
-    }
-    mean_all /= BOUND_OF_LOOP;
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        sum_temp += pow((means[i] - mean_all), 2);
-    }
-    sd = sqrt(sum_temp / BOUND_OF_LOOP);
-    printf("Overall Mean: %f\nDeviation: %f\n", mean_all, sd);
+
+    uint64_t mean = 0;
+    uint64_t variance = 0;
+    uint64_t variance_of_mean = 0;
+    uint64_t max_deviation = 0;
+    char* filename = "../stat/task_process_rst.txt";
+    do_calculation(times + 1, BOUND_OF_LOOP, SIZE_OF_STAT, &mean, &variance, &variance_of_mean, &max_deviation, filename);
+
+    printf("=========== Statistics ===========\n");
+    printf("batch size: %d, size of statistic: %d\n", BOUND_OF_LOOP, SIZE_OF_STAT);
+    printf("mean:%lu\n", mean);
+    printf("variance:%lu\n", variance);
+    printf("variance of mean:%lu\n", variance_of_mean);
+    printf("maximum deviation:%lu\n", max_deviation);
 
     printf("=========== Measuring Thread Creation ===========\n");
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        printf("Iter[%d]\n", i);
-        uint64_t sum = 0;
-        pthread_t tid;
+    for (int i = 0; i <= BOUND_OF_LOOP; i += 1) {
+        // printf("Iter[%d]\n", i);
         // unrolling required
+        pthread_t tid;
         for (int j = 0; j < SIZE_OF_STAT; j += 1) {
             __asm__ volatile (
             "cpuid\n\t"
@@ -90,25 +101,24 @@ int main() {
             );
             start = (((uint64_t)cycles_high0 << 32) | cycles_low0);
             end = (((uint64_t)cycles_high1 << 32) | cycles_low1);
-            sum += (end - start);
-            printf("%lu ", end - start);
-            if (j != 0 && j % 20 == 0) {
-                printf("\n");
+            if((end - start) < 0) {
+                printf("wrong timing: start:%lu, end:%lu ...\n", start, end);
+                times[i][j] = 0;
+            } else {
+                times[i][j] = end - start;
             }
         }
-        means[i] = (double)sum / SIZE_OF_STAT;
-        printf("\nIter[%d] mean: %f\n", i, means[i]);
     }
-    mean_all = 0;
-    sd = 0;
-    sum_temp = 0;
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        mean_all += means[i];
+    filename = "../stat/task_thread_rst.txt";
+    do_calculation(times + 1, BOUND_OF_LOOP, SIZE_OF_STAT, &mean, &variance, &variance_of_mean, &max_deviation, filename);
+    printf("=========== Statistics ===========\n");
+    printf("batch size: %d, size of statistic: %d\n", BOUND_OF_LOOP, SIZE_OF_STAT);
+    printf("mean:%lu\n", mean);
+    printf("variance:%lu\n", variance);
+    printf("variance of mean:%lu\n", variance_of_mean);
+    printf("maximum deviation:%lu\n", max_deviation);
+    for(int i = 0; i < BOUND_OF_LOOP; i++) {
+        free(times[i]);
     }
-    mean_all /= BOUND_OF_LOOP;
-    for (int i = 0; i < BOUND_OF_LOOP; i += 1) {
-        sum_temp += pow((means[i] - mean_all), 2);
-    }
-    sd = sqrt(sum_temp / BOUND_OF_LOOP);
-    printf("Overall Mean: %f\nDeviation: %f\n", mean_all, sd);
+    free(times);
 }
